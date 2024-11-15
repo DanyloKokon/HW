fec29cb40d3077540049b214e0603b2d135d84b2
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2024-11-15 16:36:35 +0100

    Yt

---
+++ HW1/JS/script.js
@@ -0,0 +1,48 @@
+const age = 13;
+console.log(age);
+
+const myName = "Dania";
+console.log(myName);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "I havent got fovorite cytata";
+console.log(myString);
+
+let myNumber = 7;
+let myNumberAnswer = 0;
+myNumberAnswer = myNumber + 10;
+console.log(myNumberAnswer);
+
+//скажу чесно в мене не вийшло зробити за допомогою тільки властивості myNumber вибачте 
+
+// let myNumber = 5; // Присвоїти довільне числове значення
+// myNumber += 10; // Додати до змінної число 10
+// console.log(myNumber); // Вивести значення myNumber в консоль
+
+// код який знизу це з чату гпт і в мене питанння чи так треба було зробити?
+
+const myNull = null;
+console.log(myNull);
+
+const askName = prompt('Fill the gap with your name');
+const greeting = "Congratulate " + askName;
+alert(greeting)
+
+const myChoose = confirm('Choose');
+console.log(myChoose)
+if (myChoose ){
+    alert("Дякую за підтвердження!");
+ } else {
+     alert("Дію відмінено!");
+  }
+
+const myLast = confirm('Вивести повідомлення про те, що дія небезпечна та попросити користувача підтвердити дію');
+console.log(myLast)
+if (myChoose ){
+    alert("Дякую за підтвердження!");
+ } else {
+     alert("Дію відмінено!");
+  }
+
---
+++ JS/rozhaluzenna.js
@@ -0,0 +1,45 @@
+const t1 = "Please enter the text";
+const t2 = "Please enter the text";
+if (t1 && t2){
+console.log("They are full");
+} else{
+    console.log("They are not full");
+}
+
+
+const n1 = 1;
+const n2 = 7;
+if(n1 + n2 > 10){
+    console.log("More than 10");
+} else{
+    console.log("Less then 10");
+}
+
+
+const tj1 = "Please enter the text";
+if(tj1.includes("JavaScript")){
+    console.log("Is JS")
+} else{
+    console.log("No JS")
+}
+
+
+const nn1 = 32;
+if(nn1 > 10 && nn1 < 20){
+    console.log("Number is in line between 10 and 20");
+} else{
+    console.log("Number is not in line between 10 and 20");
+}
+
+const nameUS = "Please enter the text";
+const mail = "try@mymail.com";
+const passcode = "Please enter the text";
+if(nameUS.length >= 3 && mail.includes("@") && mail.includes(".") && passcode.length >= 6){
+    console.log("Going to next page")
+} else{
+    console.log("Worse, pleas write with the rules")
+}
+
+
+
+//Це я вссюди написав Please enter the text бо незнав що написати))
