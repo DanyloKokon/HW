13bbe139026d8edff8bcd61ccdd6dbc72c34e338
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2024-11-30 22:02:23 +0100

    for

---
+++ JS/for.js
@@ -0,0 +1,48 @@
+// for (let i = 0; i <= 10; i+=1) {
+//    console.log(i)
+    
+// }
+
+
+
+// for (let i = 0; i < 20; i+=1) {
+//     if (i % 2 === 0) {
+//         continue
+//     } else{
+//         console.log(i);
+//     }
+    
+// }
+
+
+
+// for (let i = 0; i <= 10; i+=1) {
+//     let result = 7 * i;
+//     if (i === 7) {
+//       break;
+//     }
+//     console.log(result);
+//   }
+
+
+
+// let n = 545
+// for (let i = 0; i <= 10000; i+=1) {
+//     if (i === n) {
+//         break
+//     } else{
+//         console.log(i);
+//     }
+    
+// }
+
+
+
+for (let i = 0; i <= 20; i+=1) {
+    if (i % 3 === 0) {
+        continue
+    } else {
+      console.log(i);  
+    }
+    
+}
