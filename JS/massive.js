0fcae9d304000307269a66cd48da61fb6656d82c
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2024-12-08 14:01:40 +0100

    io9juh

---
+++ JS/massive.js
@@ -0,0 +1,53 @@
+// const number = [1, 43, 667]
+// number[1] = 10
+// console.log(number);
+
+
+
+// const sting = ['St1', 'St2', 'St3'];
+// sting[3] = 'St4';
+// console.log(sting);
+
+
+
+// const cart = [11, 62, 75, 26, 99, 23, 54]
+// let summa = 0;
+// for (const plural of cart) {
+//     summa += plural
+// }
+// console.log(summa);
+
+
+
+
+
+// const number = [2, 554, 88, 33, 22];
+// let numbers = 0;
+// for (const numbers of number) {
+//     console.log(numbers);
+// }
+
+
+
+// const sting = ['FGHJkK', 'gsjjs', 'jssssj', 'jahhs', 'jssssjs'];
+// for (const StringOff of sting) {
+//     if (StringOff.length > 5) {
+//         console.log(StringOff);
+//     }
+// }
+
+
+
+// const number = [8, 23, 15, 47, 3, 91, 56, 14, 78, 34]
+// console.log(Math.max(...number));
+
+
+
+
+
+const number = [8, 23, 15, 47, 3, 91, 56, 14, 78, 34]
+for (const numbers of number) {
+    if (numbers % 2 === 0 ) {
+        console.log(numbers);
+    }
+}
