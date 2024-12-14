50e9a9ed39f1add65ed369f52804b91ef5109f09
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2024-12-14 11:58:40 +0100

    ЧСАПРО

---
+++ JS/metodsmassive.js
@@ -0,0 +1,9 @@
+const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+let string = '';
+// for (const stt of friends) {
+//     string += stt
+
+// }
+
+let stringJoin = friends.join();
+console.log(stringJoin);
