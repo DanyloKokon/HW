8a37a143a069a1467848be3096632206dd4ad0c5
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2025-02-13 18:00:09 +0100

    dfgh

---
+++ JS/perebir2.js
@@ -0,0 +1,38 @@
+// Завдання 1\
+// Отримати масив імен всіх користувачів (поле name).
+const getUserNames = users => {
+};
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+
+
+
+// Завдання 2
+// Отримати масив об'єктів користувачів за кольором очей (поле eyeColor).
+const getUsersWithEyeColor = (users, color) => {
+
+};
+console.log(getUsersWithEyeColor(users, 'blue')); // [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
+
+
+
+
+
+// Завдання 3
+// Отримати масив імен користувачів за статтю (поле gender).
+const getUsersWithGender = (users, gender) => {
+
+};
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+
+
+
+
+// Завдання 4
+// Отримати масив тільки неактивних користувачів (поле isActive).
+const getInactiveUsers = users => {
+
+};
+console.log(getInactiveUsers(users)); // [об'єкт Moore Hensley, об'єкт Ross Vazquez, об'єкт Blackburn Dotson]
